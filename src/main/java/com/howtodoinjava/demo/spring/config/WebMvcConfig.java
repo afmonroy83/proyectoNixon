9120dafc338845f111beb167362e3652e6af59ad
afmonroy83 <felipe97.11@gmail.com>
2019-09-05 20:10:52 -0500

    medio comit

--- src/main/java/com/howtodoinjava/demo/spring/config/WebMvcConfig.java
+++ src/main/java/com/howtodoinjava/demo/spring/config/WebMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -16,6 +17,11 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.howtodoinjava.demo.spring"})
 public class WebMvcConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+            registry.addResourceHandler("/**").addResourceLocations("/");
+    }
 
    @Bean
    public InternalResourceViewResolver resolver() {
--- src/main/java/com/howtodoinjava/demo/spring/model/User.java
+++ src/main/java/com/howtodoinjava/demo/spring/model/User.java
@@ -1,5 +1,7 @@
 package com.howtodoinjava.demo.spring.model;
 
+import static com.mchange.v2.c3p0.impl.C3P0Defaults.password;
+import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,7 +15,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name = "tb_users")
-public class User {
+public class User implements Serializable {
 
    @Id
    @Column(name = "USER_ID")
@@ -28,6 +30,30 @@ public class User {
    @Email(message = "{user.email.invalid}")
    @NotEmpty(message="Please Enter your email")
    private String email;
+   
+   @Column(name = "password", unique = true)
+   @NotEmpty(message="Please Enter your password")
+   private String password;
+   
+   @Column(name = "num_contact")
+   private Long num_contact;
+   
+   @Column(name = "finc_n", unique = true)
+   private String finc_n;
+   
+   @Column(name = "Hect", unique = true)
+   private int Hect;
+   
+   @Column(name = "ener_id", unique = true)
+   private int ener_id;
+   
+   @Column(name = "estatus", unique = true)
+   private String estatus;
+   
+   @Column(name = "Direccion", unique = true)
+   private String Direccion;
+   
+   
 
    public Long getId() {
       return id;
@@ -52,4 +78,102 @@ public class User {
    public void setEmail(String email) {
       this.email = email;
    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the num_contact
+     */
+    public Long getNum_contact() {
+        return num_contact;
+    }
+
+    /**
+     * @param num_contact the num_contact to set
+     */
+    public void setNum_contact(Long num_contact) {
+        this.num_contact = num_contact;
+    }
+
+    /**
+     * @return the finc_n
+     */
+    public String getFinc_n() {
+        return finc_n;
+    }
+
+    /**
+     * @param finc_n the finc_n to set
+     */
+    public void setFinc_n(String finc_n) {
+        this.finc_n = finc_n;
+    }
+
+    /**
+     * @return the Hect
+     */
+    public int getHect() {
+        return Hect;
+    }
+
+    /**
+     * @param Hect the Hect to set
+     */
+    public void setHect(int Hect) {
+        this.Hect = Hect;
+    }
+
+    /**
+     * @return the ener_id
+     */
+    public int getEner_id() {
+        return ener_id;
+    }
+
+    /**
+     * @param ener_id the ener_id to set
+     */
+    public void setEner_id(int ener_id) {
+        this.ener_id = ener_id;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getEstatus() {
+        return estatus;
+    }
+
+    /**
+     * @param estatus the status to set
+     */
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+    /**
+     * @return the Direccion
+     */
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    /**
+     * @param Direccion the Direccion to set
+     */
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
 }
